f6a683ab554110b56b3f6bc83b6259bf26ff8678
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2024-07-15 11:21:02 -0300

    Refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,15 @@ package org.example
 import java.net.URL
 
 fun main() {
-    val apiResponse = URL("https://trakt.tv/shows/breaking-bad").readText()
+    val url = URL("https://trakt.tv/shows/breaking-bad")
+    val bannerImg = scrapeImg(url)
+    println(bannerImg)
+}
+
+fun scrapeImg(url: URL): String {
+    val apiResponse = url.readText()
     val matcher = "<section data-fanart=\"[^\"]*\"".toRegex()
     val regexFind = matcher.findAll(apiResponse).map{it.value}.toList().toString()
-    val string = regexFind.replace("[<section data-fanart=\"", "").replace("\"]", "")
-    println(string)
+    val img = regexFind.replace("[<section data-fanart=\"", "").replace("\"]", "")
+    return img
 }
